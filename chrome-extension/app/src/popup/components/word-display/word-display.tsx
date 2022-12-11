09cf3ea8c095145e4fc30088da92ffc2a2f4dbcc
Mathew_C <57735350+m4th3w561@users.noreply.github.com>
2022-12-11 11:50:29 +0900

    made text color contrast w/ its bgColor

--- chrome-extension/app/src/popup/components/word-display/word-display.tsx
+++ chrome-extension/app/src/popup/components/word-display/word-display.tsx
@@ -1,58 +1,52 @@
-import React from 'react'
-import styled from 'styled-components'
-import WordBubble from '../word-bubble/word-bubblev2'
-import WordBubbleDetail from '../word-bubble/word-bubble-detail'
+import React from "react";
+import styled from "styled-components";
+import WordBubble from "../word-bubble/word-bubblev2";
+import WordBubbleDetail from "../word-bubble/word-bubble-detail";
 
 interface WordDisplayProps {
-    onClick: () => void,
-    words: singleWord[],
-    detailWord?: any,
-    handleBubbleClick: (word: string) => void,
+	onClick: () => void;
+	words: singleWord[];
+	detailWord?: any;
+	handleBubbleClick: (word: string) => void;
 }
 
-type singleWord = [
-    string, string, number
-]
-    
+type singleWord = [string, string, number];
+
 const Container = styled.div`
-    display: flex;
-    justify-content: start;
-    align-items: center;
-    flex-flow: row wrap;
-    gap: 8px;
-    height: 198px;
-    overflow: hidden;
-`
+	display: flex;
+	justify-content: start;
+	align-items: center;
+	flex-flow: row wrap;
+	gap: 8px;
+	height: 190px;
+	overflow: scroll;
+	::-webkit-scrollbar {
+		width: 0;
+		height: 0;
+	}
+`;
 const Title = styled.div`
-    display: flex;
-    justify-content: start;
-    width: 100%;
-    font-size: 1rem;
-    font-weight: bold;
-    margin-bottom: 8px;
-`
+	display: flex;
+	justify-content: start;
+	width: 100%;
+	font-size: 1rem;
+	font-weight: bold;
+	margin-bottom: 8px;
+`;
 
-const WordDisplay = ({words, detailWord, handleBubbleClick, onClick}: WordDisplayProps) => {
+const WordDisplay = ({ words, detailWord, handleBubbleClick, onClick }: WordDisplayProps) => {
+	const bubbles = words.map((word, idx) => {
+		if (word[0] !== detailWord?.word)
+			return <WordBubble key={idx} word={word} handleBubbleClick={handleBubbleClick} />;
+		return <WordBubbleDetail key={idx} word={word} comments={detailWord?.comments} />;
+	});
 
-    const bubbles = words.map((word,idx) => {
-        if (word[0] !== detailWord?.word) return (
-            <WordBubble key={idx} word={word} handleBubbleClick={handleBubbleClick} />
-        )
-        return (
-            <WordBubbleDetail key={idx} word={word} comments={detailWord?.comments}/>
-        )
-    })
+	return (
+		<>
+			<Title>Common words</Title>
+			<Container onClick={onClick}>{bubbles}</Container>
+		</>
+	);
+};
 
-    return (
-        <>
-        <Title>Common words</Title>
-        <Container onClick={onClick}>
-            {bubbles}
-        </Container>
-        
-        </>
-        
-    )
-}
-
-export default WordDisplay
+export default WordDisplay;
