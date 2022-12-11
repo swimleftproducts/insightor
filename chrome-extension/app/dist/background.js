b5d6ecc6b59a03dbbe5174f1719a360e132ab5e8
Eric Harvey <swimleft@gmail.com>
2022-12-10 19:46:22 -0800

    test

--- chrome-extension/app/dist/background.js
+++ chrome-extension/app/dist/background.js
@@ -1,77 +1,54 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/background/background.ts":
+var __webpack_exports__ = {};
 /*!**************************************!*\
   !*** ./src/background/background.ts ***!
   \**************************************/
-/***/ (function() {
+console.log('from background script')
 
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-console.log('from background script');
-chrome.runtime.onMessage.addListener((message) => __awaiter(this, void 0, void 0, function* () {
+chrome.runtime.onMessage.addListener(async (message) => {
     if (message.type === 'get_comments') {
-        console.log('got message');
+        console.log('got message')
         const videoId = message.payload.videoId;
         // run the specified function with the provided arguments
-        const data = yield getComments(videoId);
+        const data = await getComments(videoId)
         //save results to local storage
-        sendMessageToContentScript(data, videoId);
+        sendMessageToContentScript(data,videoId)
     }
-}));
-const API_URL = 'https://sbk957ltol.execute-api.us-east-1.amazonaws.com/test/getanalysis';
-const getComments = (videoId) => __awaiter(this, void 0, void 0, function* () {
-    const fullURL = `${API_URL}?videoid=${videoId}&maxcomments=250`;
-    let data;
+  });
+
+const API_URL = 'https://sbk957ltol.execute-api.us-east-1.amazonaws.com/test/getanalysis'
+
+
+const getComments = async (videoId) => {
+    const fullURL = `${API_URL}?videoid=${videoId}&maxcomments=250`
+    let data
     try {
-        const res = yield fetch(fullURL);
+        const res = await fetch(fullURL)
         if (res.ok) {
-            data = yield res.json();
-            console.log('data from worker', data);
+            data = await res.json()
+            console.log('data from worker', data)
+        } else {
+            console.log(res)
         }
-        else {
-            console.log(res);
-        }
-        return data;
+        return data
+    } catch (error) {
+        console.log(error)
     }
-    catch (error) {
-        console.log(error);
-    }
-});
-const sendMessageToContentScript = (data, videoId) => {
+}
+
+const sendMessageToContentScript = (data,videoId) => {
     const message = {
         type: 'save_comments_data',
         payload: {
             videoId,
             data
         }
-    };
-    console.log('data in sent to content', data);
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, message);
+    }
+    console.log('data in sent to content', data)
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        chrome.tabs.sendMessage(tabs[0].id, message);  
     });
-};
-
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module is referenced by other modules so it can't be inlined
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/background/background.ts"]();
-/******/ 	
+}
 /******/ })()
 ;
 //# sourceMappingURL=background.js.map
--- chrome-extension/app/dist/contentScript.js
+++ chrome-extension/app/dist/contentScript.js
@@ -3,25 +3,30 @@ var __webpack_exports__ = {};
 /*!********************************************!*\
   !*** ./src/contentScript/contentScript.ts ***!
   \********************************************/
-console.log('in content script');
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.message === "start") {
-        start();
+console.log('in content script')
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if( request.message === "start" ) {
+            start();
+        }
     }
-});
-function start() {
-    console.log('got it');
+);
+
+function start(){
+   console.log('got it')
 }
+
 chrome.runtime.onMessage.addListener((message) => {
     if (message.type === 'save_comments_data') {
+       
         const videoId = message.payload.videoId;
-        const data = message.payload.data;
-        console.log('will save comments data', data, videoId);
-        localStorage.setItem('videoId', videoId);
-        localStorage.setItem('data', JSON.stringify(data));
+        const data = message.payload.data
+        console.log('will save comments data', data, videoId)
+        localStorage.setItem('videoId',videoId)
+        localStorage.setItem('data', JSON.stringify(data))
     }
 });
-
 /******/ })()
 ;
 //# sourceMappingURL=contentScript.js.map
